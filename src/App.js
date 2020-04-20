1164b20e39689b65451956a2dd888824b43d10ac
wiliamvj <wiliamjoaquim@gmail.com>
2020-04-20 09:33:31 -0300

    start repo in github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
+
+import './global.css';
+import '../src/assets/animation.css';
+import '../src/assets/animationTwo.css';
+import '../src/assets/animationThree.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>
+            I'm coding, wait<span className="bounceOut1">.</span><span className="bounceOut2">.</span><span className="bounceOut3">.</span>
+        </h1>
+
+        <div className="social">
+            <a href="https://github.com/wiliamvj" target="_blank" rel="noopener noreferrer">
+                <FiGithub className="icon" size={56} color="rgb(98, 114, 164)" />
+            </a>
+
+            <a href="https://br.linkedin.com/in/wiliam-joaquim-6a3068113" target="_blank" rel="noopener noreferrer">
+              <FiLinkedin className="icon" size={56} color="rgb(98, 114, 164)" />
+            </a>
+        </div>
     </div>
   );
 }
